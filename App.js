60940cdc79f61d0023973fcaa92aa077283ed44d
DiemBTran <diembtran@gmail.com>
2020-12-30 15:10:26 -0800

    Create a new React Native app
    Import Button and useState to change text upon button press

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [ outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText("You've changed me!")}/>
       <StatusBar style="auto" />
     </View>
   );
